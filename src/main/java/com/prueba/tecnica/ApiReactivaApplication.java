b5dce155bd0c97039f111981fa750739ea336a09
Denzel Zuñiga Noriega <zuniganoriegadenzel@gmail.com>
2024-09-20 13:44:22 -0500

    Initial commit

---
+++ src/main/java/com/prueba/tecnica/ApiReactivaApplication.java
@@ -0,0 +1,15 @@
+package com.prueba.tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReactivaApplication.class, args);
+
+		System.out.println("Application started");
+	}
+
+}
---
+++ src/test/java/com/prueba/tecnica/ApiReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
